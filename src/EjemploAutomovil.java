b296481cd1ca5539bfc31c7dc3464483a1ddfbc0
gustavo177 <gtvadf177@hotmail.com>
2024-04-02 11:36:51 -0500

    Implementando constructor

--- src/EjemploAutomovil.java
+++ src/EjemploAutomovil.java
@@ -1,16 +1,14 @@
 public class EjemploAutomovil {
     public static void main(String[] args) {
-        Automovil subaru = new Automovil();
-        subaru.setFabricante("Subaru");
-        subaru.setModelo("Impreza");
+        Automovil subaru = new Automovil("Subaru","Impreza");
         subaru.setCilindrada(2.0);
         subaru.setColor("Blanco");
         
-        Automovil mazda = new Automovil();
-        mazda.setFabricante("Mazda");
-        mazda.setModelo("BT-50");
+        Automovil mazda = new Automovil("Mazda","BT-50");
         mazda.setCilindrada(3.0);
         mazda.setColor("Rojo");
+
+        Automovil nissan = new Automovil();
 
         System.out.println("mazda.leerFabricante() = " + mazda.getFabricante());
 
