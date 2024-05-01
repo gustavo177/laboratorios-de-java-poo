7218fdbf9239b76653339b4689f95198e48214f9
gustavo177 <gtvadf177@hotmail.com>
2024-04-30 21:46:34 -0500

    ajustando el codigo

--- src/EjemploAutomovilArreglo.java
+++ src/EjemploAutomovilArreglo.java
@@ -37,7 +37,6 @@ public class EjemploAutomovilArreglo {
         Automovil audi = new Automovil("Audi", "A3");
         audi.setConductor((new Persona("Jano", "Pérez")));
 
-
         Automovil[] autos =  new Automovil[5];
         autos[0] = subaru;
         autos[1] = mazda;
