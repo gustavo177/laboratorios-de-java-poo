e7837212c2713f119761774eceeada1c4cceaf04
gustavo177 <gtvadf177@hotmail.com>
2024-05-05 16:17:07 -0500

    Creando el auto incremento del identificador

--- src/OrdenCompra.java
+++ src/OrdenCompra.java
@@ -1,2 +1,6 @@
 public class OrdenCompra {
+    private int identificador;
+    public int getIdentificador() {
+        return ++identificador;
+    }
 }
--- src/ProyectoOrdenCompra.java
+++ src/ProyectoOrdenCompra.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class ProyectoOrdenCompra {
     public static void main(String[] args) {
+        OrdenCompra orden = new OrdenCompra();
 
+        int identificador = orden.getIdentificador();
+        System.out.println("identificador = " + identificador);
+        System.out.println("identificador = " + orden.getIdentificador());
     }
 }
