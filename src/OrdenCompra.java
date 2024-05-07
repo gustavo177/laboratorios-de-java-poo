5e4f30d006aebe2edd6fb2071429e9d1857b8221
gustavo177 <gtvadf177@hotmail.com>
2024-05-07 12:07:04 -0500

    cree el contructor los getter y los set

--- src/OrdenCompra.java
+++ src/OrdenCompra.java
@@ -3,10 +3,40 @@ import java.util.Date;
 public class OrdenCompra {
     private int identificador;
     private String descripcion;
-    private Date fecha ;
-    private  Cliente cliente;
-    private  Producto[] productos;
+    private Date fecha;
+    private Cliente cliente;
+    private Producto[] productos;
+
     public int getIdentificador() {
         return ++identificador;
     }
+
+    public OrdenCompra(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public Producto[] getProductos() {
+        return productos;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
 }
