d5c90343a868ac01c68ed4352a3fc642cd6110cb
gustavo177 <gtvadf177@hotmail.com>
2024-05-06 20:45:37 -0500

    Creando el campo date, cliente y productos

--- src/OrdenCompra.java
+++ src/OrdenCompra.java
@@ -1,5 +1,11 @@
+import java.util.Date;
+
 public class OrdenCompra {
     private int identificador;
+    private String descripcion;
+    private Date fecha ;
+    private  Cliente cliente;
+    private  Producto[] productos;
     public int getIdentificador() {
         return ++identificador;
     }
