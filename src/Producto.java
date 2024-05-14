44fd6280ba9708bc4207d60c545dea05ffd03595
gustavo177 <gtvadf177@hotmail.com>
2024-05-14 16:22:47 -0500

    Creando el constructor

--- src/Producto.java
+++ src/Producto.java
@@ -3,6 +3,9 @@ public class Producto {
     private String nombre;
     private int precio;
 
-
-
+    public Producto(String fabricante, String nombre, int precio) {
+        this.fabricante = fabricante;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
 }
