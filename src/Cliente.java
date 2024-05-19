c13d207cff5ca409f1b76dec979da5a681fb7714
gustavo177 <gtvadf177@hotmail.com>
2024-05-19 16:03:35 -0500

    Creando el constructor

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,4 +1,9 @@
 public class Cliente {
     private String nombre;
     private String apellido;
+
+    public Cliente(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
 }
