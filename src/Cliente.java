10dd667d5a5abfaa69b139edb21fe64974941050
gustavo177 <gtvadf177@hotmail.com>
2024-05-20 19:47:06 -0500

    creando los getter

--- src/Cliente.java
+++ src/Cliente.java
@@ -6,4 +6,12 @@ public class Cliente {
         this.nombre = nombre;
         this.apellido = apellido;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
 }
